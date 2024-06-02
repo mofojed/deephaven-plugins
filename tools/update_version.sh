9dcc33fa8f4cfdd0307c48019da2ce8a2cd5efd2
Mike Bender <mikebender@deephaven.io>
2024-06-02 16:49:53 -0400

    WIP create the deephaven.ui experimental plugin
    - Will be used for experimental plugins that we can publish

--- tools/update_version.sh
+++ tools/update_version.sh
@@ -113,7 +113,7 @@ function update_file() {
 extra=
 [ "$dev" = true ] && extra=".dev0"
 case "$package" in
-        json | matplotlib | plotly | plotly-express | ui | utilities | packaging)
+        json | matplotlib | plotly | plotly-express | ui | ui-experimental | utilities | packaging)
             update_file "${package}/setup.cfg" 'version = ' '' "$extra"
             ;;
         auth-keycloak | dashboard-object-viewer | table-example)
@@ -130,7 +130,7 @@ esac
 npm_version="${version}"
 if [ "$dev" != true ]; then
     case "$package" in
-        auth-keycloak | dashboard-object-viewer | matplotlib | plotly | plotly-express | table-example | ui)
+        auth-keycloak | dashboard-object-viewer | matplotlib | plotly | plotly-express | table-example | ui | ui-experimental)
             # The working directory is already `plugins/<package-name>`, so we just specify workspace as `src/js` and it does the right thing
             npm version "$npm_version" --workspace=src/js
             ;;
