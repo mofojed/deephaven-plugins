6441146239fd08727ae73a16955ff3ed26f94e23
mikebender <mikebender@deephaven.io>
2024-06-14 11:05:47 -0400

    Clean up errors and how they're displayed
    - Conform WidgetError to Error type

--- plugins/ui/src/js/src/widget/WidgetUtils.tsx
+++ plugins/ui/src/js/src/widget/WidgetUtils.tsx
@@ -11,11 +11,7 @@ import {
   Section,
 } from '@deephaven/components';
 import { ValueOf } from '@deephaven/utils';
-import {
-  ReadonlyWidgetData,
-  WidgetAction,
-  isWidgetAction,
-} from './WidgetTypes';
+import { ReadonlyWidgetData, WidgetAction, isWidgetError } from './WidgetTypes';
 import {
   ElementNode,
   ELEMENT_KEY,
@@ -122,13 +118,8 @@ export function getPreservedData(
  * @returns The name of the error
  */
 export function getErrorName(error: unknown): string {
-  if (
-    typeof error === 'object' &&
-    error != null &&
-    'name' in error &&
-    typeof error.name === 'string'
-  ) {
-    return error.constructor.name;
+  if (isWidgetError(error)) {
+    return error.name;
   }
   return 'Unknown error';
 }
@@ -139,12 +130,7 @@ export function getErrorName(error: unknown): string {
  * @returns The error message
  */
 export function getErrorMessage(error: unknown): string {
-  if (
-    typeof error === 'object' &&
-    error != null &&
-    'message' in error &&
-    typeof error.message === 'string'
-  ) {
+  if (isWidgetError(error)) {
     return error.message.trim();
   }
   return 'Unknown error';
@@ -167,13 +153,8 @@ export function getErrorShortMessage(error: unknown): string {
  * @returns The error stack trace
  */
 export function getErrorStack(error: unknown): string {
-  if (
-    typeof error === 'object' &&
-    error != null &&
-    'stack' in error &&
-    typeof error.stack === 'string'
-  ) {
-    return error.stack;
+  if (isWidgetError(error)) {
+    return error.stack ?? '';
   }
   return '';
 }
@@ -184,13 +165,8 @@ export function getErrorStack(error: unknown): string {
  * @returns The action from the error, if it exists
  */
 export function getErrorAction(error: unknown): WidgetAction | null {
-  if (
-    typeof error === 'object' &&
-    error != null &&
-    'action' in error &&
-    isWidgetAction(error.action)
-  ) {
-    return error.action;
+  if (isWidgetError(error)) {
+    return error.action ?? null;
   }
   return null;
 }
