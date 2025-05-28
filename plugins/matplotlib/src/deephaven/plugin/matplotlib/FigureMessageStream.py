e0cc1a936a158cc2d0ee981695705d982c72e0a4
mikebender <mikebender@deephaven.io>
2025-05-28 09:46:06 -0400

    Try with a throttle?

--- plugins/matplotlib/src/deephaven/plugin/matplotlib/FigureMessageStream.py
+++ plugins/matplotlib/src/deephaven/plugin/matplotlib/FigureMessageStream.py
@@ -11,6 +11,26 @@ logger = logging.getLogger(__name__)
 
 DPI = 144
 
+import time
+from functools import wraps
+
+
+def throttle(seconds):
+    def decorator(func):
+        last_called = 0
+
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            nonlocal last_called
+            current_time = time.time()
+            if current_time - last_called >= seconds:
+                last_called = current_time
+                return func(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
+
 
 class FigureMessageStream(MessageStream):
     _figure: Figure
@@ -34,11 +54,40 @@ class FigureMessageStream(MessageStream):
     def _handle_figure_update(self, artist, stale: bool) -> None:
         # Check if we're already drawing this figure, and the stale callback was triggered because of our call to savefig
         if self._is_exporting:
-            print("Figure is already exporting, skipping update")
+            # print("Figure is already exporting, skipping update")
             return
 
-        print("Figure update detected, exporting figure")
+        self._send_update()
+
+        # self._is_exporting = True
+
+        # # Print the stack trace
+        # import traceback
+
+        # # print("Figure update detected, handling update")
+        # traceback.print_stack()
+
+        # print("Figure update detected, exporting figure")
+        # buf = BytesIO()
+
+        # print("Saving figure to buffer")
+        # self._figure.savefig(
+        #     buf, format="PNG", dpi=DPI
+        # )  # Save the figure to the buffer
+        # self._connection.on_data(buf.getvalue(), [])
+        # print("Figure saved to buffer and sent to client")
+
+        # self._is_exporting = False
+
+    @throttle(0.5)  # Throttle updates to every 0.5 seconds
+    def _send_update(self) -> None:
+        """
+        Send an update to the client. This is throttled to avoid sending too many updates in a short period of time.
+        """
+        if self._is_exporting:
+            return
         self._is_exporting = True
+
         buf = BytesIO()
 
         print("Saving figure to buffer")
