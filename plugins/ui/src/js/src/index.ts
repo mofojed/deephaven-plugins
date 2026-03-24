38479c96826d73fb95e670487370a1ea4bc0405e
Mike Bender <mikebender@deephaven.io>
2026-03-23 23:00:34 -0400

    Add MultiPlugin support for deephaven.ui
    - Add a DashboardPlugin which is used for legacy purposes

--- plugins/ui/src/js/src/index.ts
+++ plugins/ui/src/js/src/index.ts
@@ -1,5 +1,23 @@
-import UIWidgetPlugin from './UIWidgetPlugin';
+import { PluginType } from '@deephaven/plugin';
+import { UIWidgetPlugin } from './UIWidgetPlugin';
+import { DashboardPlugin } from './DashboardPlugin';
 
-export * from './DashboardPlugin';
+// TODO: Remove local MultiPlugin type once @deephaven/plugin is updated
+// to include it (see deephaven/web-client-ui composite-plugins branch)
+const MULTI_PLUGIN = 'MultiPlugin' as const;
 
-export default UIWidgetPlugin;
+const UIDashboardPlugin = {
+  name: '@deephaven/js-plugin-ui.DashboardPlugin',
+  type: PluginType.DASHBOARD_PLUGIN as string,
+  component: DashboardPlugin,
+};
+
+const UIMultiPlugin = {
+  name: '@deephaven/js-plugin-ui',
+  type: MULTI_PLUGIN as string,
+  plugins: [UIWidgetPlugin, UIDashboardPlugin],
+};
+
+export { DashboardPlugin };
+
+export default UIMultiPlugin;
